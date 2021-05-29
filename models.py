e818097d082bab40f9a14fcc5b1f1793b1f2992b
Tsekho <scalfaro18@gmail.com>
2021-05-29 17:40:51 +0300

    major rework

--- models.py
+++ models.py
@@ -3,6 +3,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+"""
+ResNet in Pytorch.
+[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun.
+    Deep Residual Learning for Image Recognition
+    https://arxiv.org/abs/1512.03385v1
+"""
+
+
 class BasicBlock(nn.Module):
     expansion = 1
 
@@ -17,11 +25,9 @@ class BasicBlock(nn.Module):
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion * planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion * planes,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion * planes)
-            )
+            self.shortcut = nn.Sequential(nn.Conv2d(in_planes, self.expansion * planes,
+                                                    kernel_size=1, stride=stride, bias=False),
+                                          nn.BatchNorm2d(self.expansion * planes))
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
@@ -95,6 +101,138 @@ class ResNet(nn.Module):
         return out
 
 
+"""
+SimplerNetV1 in Pytorch.
+[1] The implementation is basded on:
+    https://github.com/D-X-Y/ResNeXt-DenseNet
+"""
+
+
+class SimpleNet(nn.Module):
+    def __init__(self, classes=10):
+        super(SimpleNet, self).__init__()
+        self.features = self._make_layers()
+        self.classifier = nn.Linear(256, classes)
+        self.drp = nn.Dropout(0.1)
+
+    def forward(self, x):
+        out = self.features(x)
+
+        out = F.max_pool2d(out, kernel_size=out.size()[2:])
+        out = self.drp(out)
+
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self):
+        model = nn.Sequential(nn.Conv2d(3, 64, kernel_size=[3, 3],
+                                        stride=(1, 1), padding=(1, 1)),
+                              nn.BatchNorm2d(
+                                  64, eps=1e-05, momentum=0.05, affine=True),
+                              nn.ReLU(inplace=True),
+
+                              nn.Conv2d(64, 128, kernel_size=[3, 3],
+                                        stride=(1, 1), padding=(1, 1)),
+                              nn.BatchNorm2d(
+                                  128, eps=1e-05, momentum=0.05, affine=True),
+                              nn.ReLU(inplace=True),
+
+                              nn.Conv2d(128, 128, kernel_size=[
+                                  3, 3], stride=(1, 1), padding=(1, 1)),
+                              nn.BatchNorm2d(
+                                  128, eps=1e-05, momentum=0.05, affine=True),
+                              nn.ReLU(inplace=True),
+
+                              nn.Conv2d(128, 128, kernel_size=[
+                                  3, 3], stride=(1, 1), padding=(1, 1)),
+                              nn.BatchNorm2d(
+                                  128, eps=1e-05, momentum=0.05, affine=True),
+                              nn.ReLU(inplace=True),
+
+
+                              nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2),
+                                           dilation=(1, 1), ceil_mode=False),
+                              nn.Dropout2d(p=0.1),
+
+                              nn.Conv2d(128, 128, kernel_size=[
+                                  3, 3], stride=(1, 1), padding=(1, 1)),
+                              nn.BatchNorm2d(
+                                  128, eps=1e-05, momentum=0.05, affine=True),
+                              nn.ReLU(inplace=True),
+
+                              nn.Conv2d(128, 128, kernel_size=[
+                                  3, 3], stride=(1, 1), padding=(1, 1)),
+                              nn.BatchNorm2d(
+                                  128, eps=1e-05, momentum=0.05, affine=True),
+                              nn.ReLU(inplace=True),
+
+                              nn.Conv2d(128, 256, kernel_size=[
+                                  3, 3], stride=(1, 1), padding=(1, 1)),
+                              nn.BatchNorm2d(
+                                  256, eps=1e-05, momentum=0.05, affine=True),
+                              nn.ReLU(inplace=True),
+
+                              nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2),
+                                           dilation=(1, 1), ceil_mode=False),
+                              nn.Dropout2d(p=0.1),
+
+                              nn.Conv2d(256, 256, kernel_size=[
+                                  3, 3], stride=(1, 1), padding=(1, 1)),
+                              nn.BatchNorm2d(
+                                  256, eps=1e-05, momentum=0.05, affine=True),
+                              nn.ReLU(inplace=True),
+
+                              nn.Conv2d(256, 256, kernel_size=[
+                                  3, 3], stride=(1, 1), padding=(1, 1)),
+                              nn.BatchNorm2d(
+                                  256, eps=1e-05, momentum=0.05, affine=True),
+                              nn.ReLU(inplace=True),
+
+                              nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2),
+                                           dilation=(1, 1), ceil_mode=False),
+                              nn.Dropout2d(p=0.1),
+
+                              nn.Conv2d(256, 512, kernel_size=[
+                                  3, 3], stride=(1, 1), padding=(1, 1)),
+                              nn.BatchNorm2d(
+                                  512, eps=1e-05, momentum=0.05, affine=True),
+                              nn.ReLU(inplace=True),
+
+                              nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2),
+                                           dilation=(1, 1), ceil_mode=False),
+                              nn.Dropout2d(p=0.1),
+
+                              nn.Conv2d(512, 2048, kernel_size=[
+                                  1, 1], stride=(1, 1), padding=(0, 0)),
+                              nn.BatchNorm2d(2048, eps=1e-05,
+                                             momentum=0.05, affine=True),
+                              nn.ReLU(inplace=True),
+
+                              nn.Conv2d(2048, 256, kernel_size=[
+                                  1, 1], stride=(1, 1), padding=(0, 0)),
+                              nn.BatchNorm2d(
+                                  256, eps=1e-05, momentum=0.05, affine=True),
+                              nn.ReLU(inplace=True),
+
+                              nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2),
+                                           dilation=(1, 1), ceil_mode=False),
+                              nn.Dropout2d(p=0.1),
+
+                              nn.Conv2d(256, 256, kernel_size=[
+                                  3, 3], stride=(1, 1), padding=(1, 1)),
+                              nn.BatchNorm2d(
+                                  256, eps=1e-05, momentum=0.05, affine=True),
+                              nn.ReLU(inplace=True))
+
+        for m in model.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.xavier_uniform_(
+                    m.weight.data, gain=nn.init.calculate_gain('relu'))
+
+        return model
+
+
 def ResNet18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
 
@@ -105,3 +243,7 @@ def ResNet34():
 
 def ResNet50():
     return ResNet(Bottleneck, [3, 4, 6, 3])
+
+
+def SimpleNetV1():
+    return SimpleNet()
